f71b033c2442d2398fc20fc4081cf06663977303
Yamashi <yamashi@live.com>
2013-04-23 17:58:22 +0200

    - Started to write interfaces for the API

---
+++ Src/Game.API/GlobalFactory.cs
@@ -0,0 +1,32 @@
+﻿using Game.API.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.API
+{
+    public static class GlobalFactory
+    {
+        private static IModule m_module = null;
+
+        public IModule Module
+        {
+            get
+            {
+                if (m_module == null)
+                {
+                    Module mod = Assembly.GetExecutingAssembly().GetModules()[0];
+
+                    if (mod.Name == "Oblivion.exe")
+                        m_module = new OblivionModule();
+                    else if (mod.Name == "TESV.exe")
+                        m_module = new SkyrimModule();
+                }
+                return m_module;
+            }
+        }
+    }
+}
