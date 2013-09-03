1c8829f3e926187b46f1ae1fc138cf289393fbab
Nerus87 <konradjanczarek@gmail.com>
2013-09-03 13:38:52 +0200

    Added IniManager to Game.Tools for reading ini files based on Yamashi code

---
+++ Src/Game.Tools/IniManager/IniLoader.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using IniParser;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using System.Reflection;
+
+namespace Game.Tools.IniManager
+{
+    public class IniLoader
+    {
+
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private IniData data = null;
+
+        public IniLoader(string pathToIniFile)
+        {
+
+            try
+            {
+                IniParser.FileIniDataParser parser = new IniParser.FileIniDataParser();
+                data = parser.LoadFile("GameServer.ini");
+            }
+            catch (System.Exception ex)
+            {
+                Logger.Error(ex.Message);
+            }
+            
+        }
+
+        /// <summary>
+        /// EN: Method return String value from config
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="defaultString"></param>
+        /// <returns></returns>
+        public string GetValue(string section, string key, string defaultString)
+        {
+            if (data != null)
+            {
+                var general = data.Sections.GetSectionData(section);
+                if (general != null)
+                {
+                    if (general.Keys.ContainsKey(key))
+                    {
+                        var keyData = general.Keys.GetKeyData(key);
+                        if (keyData != null && keyData.Value.Length > 0)
+                        {
+                            return keyData.Value;
+                        }
+                    }
+                }
+            }
+            return defaultString;
+        }
+
+        /// <summary>
+        /// EN: Method return Int value from config
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public int GetValue(string section, string key, int defaultValue)
+        {
+            if (data != null)
+            {
+                var general = data.Sections.GetSectionData(section);
+                if (general != null)
+                {
+                    if (general.Keys.ContainsKey(key))
+                    {
+                        var keyData = general.Keys.GetKeyData(key);
+                        if (keyData != null && keyData.Value.Length > 0)
+                        {
+                            return int.Parse(keyData.Value);
+                        }
+                    }
+                }
+            }
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// EN: Method return Boolean value from config
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public bool GetValue(string section, string key, bool defaultValue)
+        {
+            if (data != null)
+            {
+                var general = data.Sections.GetSectionData(section);
+                if (general != null)
+                {
+                    if (general.Keys.ContainsKey(key))
+                    {
+                        var keyData = general.Keys.GetKeyData(key);
+                        if (keyData != null && (keyData.Value.Length == 4 || keyData.Value.Length == 5))
+                        {
+                            return bool.Parse(keyData.Value.ToLower());
+                        }
+                    }
+                }
+            }
+            return defaultValue;
+        }
+
+
+    }
+}
---
+++ Src/Game.Tools/IniManager/IniWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using IniParser;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Tools.IniManager
+{
+    class IniWriter
+    {
+        static void write()
+        {
+            IniParser.FileIniDataParser fidp = new FileIniDataParser();
+           // fidp.
+
+        }
+    }
+}
