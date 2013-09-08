9e6c9627eb3b986c5f6005f439cc9f49f4ff60e3
Nerus87 <konradjanczarek@gmail.com>
2013-09-08 14:37:20 +0200

    GameTools: -GameTools.cs : --added version and compile date -IniLoader.cs : --added method  GetValues from collection (not tested yet) --added method GetValueContainer to get specific types: string, bool, int, as collection from GetValues collection (not tested yet)

--- Src/Game.Tools/GameTools.cs
+++ Src/Game.Tools/GameTools.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Game.Tools.IniManager;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Game.Tools
 {
+
     public class GameTools
     {
+        private static readonly string Version = "0.0.5";
+        private static IniLoader loader = null;
+        private static IniWriter writer = null;
+        public static readonly string compileDate = DateTime.Now.ToString();
     }
+
 }
