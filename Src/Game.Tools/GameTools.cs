6f80060c5fbfbaa3910ff6fd5b03f7f030d9cf08
Nerus87 <konradjanczarek@gmail.com>
2013-09-25 20:45:23 +0200

    Add image schemas and some changes in project

--- Src/Game.Tools/GameTools.cs
+++ Src/Game.Tools/GameTools.cs
@@ -10,8 +10,6 @@ namespace Game.Tools
     public class GameTools
     {
         private static readonly string Version = "0.0.5";
-        private static IniLoader loader = null;
-        private static IniWriter writer = null;
 
         // I doubt this has the expected behavior
         public static readonly string CompileDate = DateTime.Now.ToString();
