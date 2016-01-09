79550fdd9b30b043d484386d4c26ff17731efc26
Goose <chrisriker@outlook.com>
2016-01-09 11:56:42 -0600

    Changed some script things
    Changed codegen.bat to not use hard-coded directories.
    Added a script to set your PythonDir environment variable, so that you
    can always run codegen.bat without having to provide it with an extra
    argument or having to change the script every time a change is made to
    it in the master branch.

--- Build/codegen.bat
+++ Build/codegen.bat
@@ -1,4 +1,4 @@
 @echo off
-E:\Software\Python\python.exe E:\Projects\C++\SkyrimOnline\Build\codegen.py ../Code/messages/ Protocol.xml Cli Game Client
-E:\Software\Python\python.exe E:\Projects\C++\SkyrimOnline\Build\codegen.py ../Code/messages/ Protocol.xml Game Cli Server
+"%PythonDir%/\python.exe" ./codegen.py ../Code/messages/ Protocol.xml Cli Game Client
+"%PythonDir%/\python.exe" ./codegen.py ../Code/messages/ Protocol.xml Game Cli Server
 pause
